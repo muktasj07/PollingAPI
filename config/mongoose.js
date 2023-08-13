01c858d480d0a726809ab0b073ab10086945fa6a
muktasj07 <jmukta272@gmail.com>
2023-08-13 15:43:37 +0530

    added

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const DB = 'mongodb+srv://muktasj0721:12345@cluster0.zcot0cp.mongodb.net/PollingAPI?retryWrites=true&w=majority';
+mongoose.connect(DB).then(()=>{
+    console.log('Connection successful!');
+}).catch((err) => console.log("no connection " + err));
+
+//setting it to db
+const db = mongoose.connection;
+
+/** ------------------ CHECKING CONNECTION ------------------ **/
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+module.exports = db;
